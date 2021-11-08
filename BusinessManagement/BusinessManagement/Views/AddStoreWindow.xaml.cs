6710c05505b1a76546f8c784bce0274d56f5fbfe
Nguyễn Thành Trung <20521015@gm.uit.edu.vn>
2021-11-08 20:27:35 +0700

    Fix bug
    Fix bug in Views, reposition files.

---
+++ BusinessManagement/BusinessManagement/Views/AddStoreWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusinessManagement.Views
+{
+    /// <summary>
+    /// Interaction logic for AddStoreWindow.xaml
+    /// </summary>
+    public partial class AddStoreWindow : Window
+    {
+        public AddStoreWindow()
+        {
+            InitializeComponent();
+
+            this.txtName.Text = null;
+            this.txtAddress.Text = null;
+            this.txtPhone.Text = null;
+            this.txtEmail.Text = null;
+            this.dpCheckin.SelectedDate = null;
+            this.txtDebt.Text = null;
+            this.cbbSpecies.Text = null;
+            this.cbDistrict.Text = null;
+        }
+    }
+}
